3093506d29508801a6eeaf28cdbc46f9b86f7394
Paras Gambhir <paras5217@gmail.com>
2022-09-12 23:32:16 +0530

    1st commit

---
+++ build/precache-manifest.c4771c063fc5e26fd8370ddb8deca016.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf9096c6ba46abda951064932f1b7b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c767d197c2b26ece2264",
+    "url": "/static/css/main.2f673a8b.chunk.css"
+  },
+  {
+    "revision": "a84c19c99ea30c0812a7",
+    "url": "/static/js/2.462f5de2.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.462f5de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c767d197c2b26ece2264",
+    "url": "/static/js/main.3f0ae44f.chunk.js"
+  },
+  {
+    "revision": "828c0bdfa56a628b17a7",
+    "url": "/static/js/runtime-main.eb9be228.js"
+  },
+  {
+    "revision": "ef02fd95b5c1f835dcbb42e16b024bbd",
+    "url": "/static/media/logo.ef02fd95.png"
+  }
+]);
